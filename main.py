a534c85e120ebc0ad7f66b1db2fae909d11f32bc
EZCode45 <eishan.hinduja@gmail.com>
2025-07-04 11:02:14 -0400

    created player and got it to jump

---
+++ main.py
@@ -0,0 +1,23 @@
+import pyxel
+pyxel.init(200, 200, 'GEOMETRYDASH', 90)
+pyxel.mouse(True)
+player_x = 95
+player_y = 150
+gravity = 0.5
+def update():
+    player_y_vel = 0
+    pyxel.cls(10)
+    global player_x, player_y, gravity
+    player_y += gravity
+    if player_y >= 150:
+        gravity = 0
+        if pyxel.btn(pyxel.KEY_SPACE):
+            player_y_vel -= 20
+    else:
+        gravity = 0.5
+    player_y += player_y_vel
+def draw():
+    global player_x, player_y
+    # pyxel.rect(0, 0, 200, 200, col = 1)
+    pyxel.rect(player_x, player_y, 10, 10, col = 20)
+pyxel.run(update, draw)
